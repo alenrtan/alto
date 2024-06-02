17a742a7e316a4e5871f6da8cfc01342f66d3a46
Alen Ray <113738058+alenrtan@users.noreply.github.com>
2024-06-01 21:23:57 -0600

    additional set up

--- index.ts
+++ index.ts
@@ -1,6 +1,57 @@
-function sayHello(name: string) {
-    console.log(`Hello, ${name}!`);
-  }
-  
-  const user = "Name";
-  sayHello(user);
+/*
+	CODE AQUIRED FROM OPEN-METEO
+*/
+
+import { fetchWeatherApi } from 'openmeteo';
+	
+const params = {
+	"latitude": 52.52,
+	"longitude": 13.41,
+	"hourly": ["temperature_2m", "relative_humidity_2m", "dew_point_2m", "apparent_temperature", "precipitation_probability", "precipitation"],
+	"temperature_unit": "fahrenheit",
+	"wind_speed_unit": "mph",
+	"precipitation_unit": "inch"
+};
+const url = "https://api.open-meteo.com/v1/forecast";
+
+(async function getWeather(){
+	const responses = await fetchWeatherApi(url, params);
+
+// Helper function to form time ranges
+const range = (start: number, stop: number, step: number) =>
+	Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+// Process first location. Add a for-loop for multiple locations or weather models
+const response = responses[0];
+
+// Attributes for timezone and location
+const utcOffsetSeconds = response.utcOffsetSeconds();
+const timezone = response.timezone();
+const timezoneAbbreviation = response.timezoneAbbreviation();
+const latitude = response.latitude();
+const longitude = response.longitude();
+
+const hourly = response.hourly()!;
+
+// Note: The order of weather variables in the URL query and the indices below need to match!
+const weatherData = {
+
+	hourly: {
+		time: range(Number(hourly.time()), Number(hourly.timeEnd()), hourly.interval()).map(
+			(t) => new Date((t + utcOffsetSeconds) * 1000)
+		),
+		temperature2m: hourly.variables(0)!.valuesArray()!,
+	},
+
+};
+
+// `weatherData` now contains a simple structure with arrays for datetime and weather data
+for (let i = 0; i < weatherData.hourly.time.length; i++) {
+	console.log(
+		weatherData.hourly.time[i].toISOString(),
+		weatherData.hourly.temperature2m[i]
+	);
+}
+})() // end of async function
+
+
